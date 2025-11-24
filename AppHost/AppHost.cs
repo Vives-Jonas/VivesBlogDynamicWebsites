787f6686b27b74ef50a297ada68ad0115e198a12
JonasVanluchene <jonas.vanluchene@gmail.com>
2025-11-24 12:01:15 +0100

    Setup basic Blazor App

--- AppHost/AppHost.cs
+++ AppHost/AppHost.cs
@@ -3,6 +3,7 @@ using Projects;
 var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<VivesBlog_Api>("API");
+builder.AddProject<VivesBlog_Blazor>("Blazor");
 builder.AddProject<VivesBlog_Mvc>("MVC");
 
 builder.Build().Run();
